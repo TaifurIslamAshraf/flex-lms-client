5966021c82521ee6890ec7a98c550ab33abbdb5c
Taifur <taifurislamashraf@gmail.com>
2024-06-04 09:52:57 +0600

    [add] footer

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/layout/sections/Banner";
+import Footer from "@/components/layout/sections/Footer";
 
 export default function Home() {
   return (
     <main className="">
       <Banner />
+      <Footer />
     </main>
   );
 }
