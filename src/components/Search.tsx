4119402dae2ba03d9c3e7030dc79ecb104a9604c
md taifur <taifurislamashraf@gmail.com>
2024-06-14 12:05:02 +0600

    [add] search functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,40 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const Search = () => {
+  const router = useRouter();
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (searchText) {
+      router.push(`/courses?search=${searchText}`);
+    }
+  };
+
   return (
-    <div className="flex items-center">
-      <SearchIcon
-        fillRule="evenodd"
-        clipRule="evenodd"
-        className="absolute z-30 ml-4 text-primary placeholder:font-siliguri"
-      />
+    <form className="flex items-center" onSubmit={handleSearch}>
+      <Button
+        type="submit"
+        variant={"ghost"}
+        size={"icon"}
+        className="absolute z-30 text-primary placeholder:font-siliguri"
+      >
+        <SearchIcon fillRule="evenodd" clipRule="evenodd" className="" />
+      </Button>
       <Input
         className="relative pl-12 max-w-[400px] min-w-[400px] w-full"
         name="search"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
         placeholder="আপনার কাঙ্খিত কোর্সটি সার্চ করুন"
       />
-    </div>
+    </form>
   );
 };
 
