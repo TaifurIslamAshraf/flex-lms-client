e0fe3e50b9b50d244c1aea6ca292f0d9faeea452
md taifur <taifurislamashraf@gmail.com>
2024-06-14 10:34:35 +0600

    [fix] confilict

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import { LoadingButton } from "@/components/LoaderButton";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -47,7 +46,6 @@ const Login = () => {
   const session = useSession();
   const dispatch = useDispatch();
   const session = useSession();
-  const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm<z.infer<typeof loginFormSchema>>({
     resolver: zodResolver(loginFormSchema),
@@ -124,13 +122,9 @@ const Login = () => {
                 <Link href={"/forgotPassword"}>Forgot Password?</Link>
               </div>
 
-              {isLoading ? (
-                <LoadingButton className="w-full" />
-              ) : (
-                <Button className="w-full" type="submit">
-                  Sign In
-                </Button>
-              )}
+              <Button className="w-full" type="submit">
+                Sign In
+              </Button>
             </form>
           </Form>
         </CardContent>
