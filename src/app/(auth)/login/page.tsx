639fcb371fa6bdecce7249e43a5aba24b27a82ad
md taifur <taifurislamashraf@gmail.com>
2024-06-14 12:20:57 +0600

    [fix] login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -44,8 +44,6 @@ const Login = () => {
 
   const dispatch = useDispatch();
   const session = useSession();
-  const dispatch = useDispatch();
-  const session = useSession();
 
   const form = useForm<z.infer<typeof loginFormSchema>>({
     resolver: zodResolver(loginFormSchema),
